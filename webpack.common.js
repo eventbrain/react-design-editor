2bfb292be538934b7474873970f2ee46e2d1630c
Andres Mestra <andres-mestra@users.noreply.github.com>
2022-01-05 18:02:12 -0500

    build: implementation ts-loader in dev mode

--- webpack.common.js
+++ webpack.common.js
@@ -5,47 +5,14 @@ module.exports = {
 		rules: [
 			{
 				test: /\.(js|jsx|tsx|ts)$/,
-				loader: 'babel-loader',
 				include: path.resolve(__dirname, 'src'),
-				options: {
-					cacheDirectory: true,
-					babelrc: false,
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false,
-								useBuiltIns: 'usage',
-								corejs: 3,
-								targets: { browsers: ['last 5 versions', 'ie >= 11'], node: 'current' },
-							},
-						],
-						'@babel/preset-react',
-						[
-							'@babel/preset-typescript',
-							{
-								isTSX: true,
-								allExtensions: true,
-								allowDeclareFields: true,
-							},
-						],
-					],
-					plugins: [
-						'@babel/plugin-transform-runtime',
-						['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
-						['@babel/plugin-proposal-class-properties', { loose: true }],
-						['@babel/plugin-proposal-private-methods', { loose: true }],
-						['@babel/plugin-proposal-decorators', { legacy: true }],
-						['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-						'@babel/plugin-syntax-dynamic-import',
-						'@babel/plugin-syntax-async-generators',
-						'@babel/plugin-proposal-object-rest-spread',
-						'react-hot-loader/babel',
-						'dynamic-import-webpack',
-						['import', { libraryName: 'antd', style: true }],
-					],
-				},
 				exclude: /node_modules/,
+				use: {
+					loader: 'ts-loader',
+					options: {
+						transpileOnly: true,
+					},
+				},
 			},
 			{
 				test: /\.(css|less)$/,
