8b3444faeab29e3d6a50cfca01d8be4028959c61
Andres Mestra <andres-mestra@users.noreply.github.com>
2022-01-07 09:12:18 -0500

    feat: add hook form update

---
+++ src/hooks/useSetFieldFormValue.js
@@ -0,0 +1,8 @@
+export const useSetFieldFormValue = form => {
+	const onChangeInput = e => {
+		const value = e.target?.value;
+		const key = e.target.id ?? e.target?.name;
+		form.setFieldsValue({ [key]: value });
+	};
+	return [onChangeInput];
+};
