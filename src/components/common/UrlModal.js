071d19f797ec684a7a5ad0bffd94ad2c76a1dff5
Andres Mestra <andres-mestra@users.noreply.github.com>
2022-01-07 09:14:58 -0500

    fix: set canvas background

--- src/components/common/UrlModal.js
+++ src/components/common/UrlModal.js
@@ -1,99 +1,91 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Modal, Button, Input } from 'antd';
 import i18n from 'i18next';
 
 import Icon from '../icon/Icon';
 
-class UrlModal extends Component {
-	handlers = {
-		onOk: () => {
-			const { onChange } = this.props;
-			const { tempUrl } = this.state;
-			onChange(tempUrl);
-			this.setState({
-				visible: false,
-				url: tempUrl,
-			});
-		},
-		onCancel: () => {
-			this.modalHandlers.onHide();
-		},
-		onClick: () => {
-			this.modalHandlers.onShow();
-		},
-	};
+const UrlModal = ({onChange, value}) => {
+  const [state, setState] = useState({
+    url: value || '',
+    tempUrl: value || '',
+    visible: false,
+  });
 
-	modalHandlers = {
+  const onOk = () => {
+    const { tempUrl } = state;
+    onChange(tempUrl);
+    setState({
+      ...state,
+      visible: false,
+      url: tempUrl,
+    });
+  }
+
+  const onCancel = () => {
+    modalHandlers.onHide();
+  }
+
+  const onClick = () => {
+    modalHandlers.onShow();
+  },
+
+
+	const modalHandlers = {
 		onShow: () => {
-			this.setState({
+			setState({
+        ...state,
 				visible: true,
 			});
 		},
 		onHide: () => {
-			this.setState({
+			setState({
+        ...state,
 				visible: false,
 			});
 		},
 	};
 
-	static propTypes = {
-		value: PropTypes.any,
-		onChange: PropTypes.func,
-		form: PropTypes.any,
-	};
+  const label = (
+    <>
+      <span style={{ marginRight: 8 }}>{i18n.t('common.url')}</span>
+      <Button onClick={onClick} shape="circle" className="rde-action-btn">
+        <Icon name="edit" />
+      </Button>
+    </>
+  );
 
-	state = {
-		url: this.props.value || '',
-		tempUrl: '',
-		visible: false,
-	};
-
-	UNSAFE_componentWillReceiveProps(nextProps) {
-		this.setState({
-			url: nextProps.value || '',
-		});
-	}
-
-	render() {
-		const { onOk, onCancel, onClick } = this.handlers;
-		const { form } = this.props;
-		const { getFieldDecorator } = form;
-		const { url, visible } = this.state;
-		const label = (
-			<React.Fragment>
-				<span style={{ marginRight: 8 }}>{i18n.t('common.url')}</span>
-				<Button onClick={onClick} shape="circle" className="rde-action-btn">
-					<Icon name="edit" />
-				</Button>
-			</React.Fragment>
-		);
-		return (
-			<React.Fragment>
-				<Form.Item label={label} colon={false}>
-					{getFieldDecorator('url', {
-						rules: [
-							{
-								required: true,
-								message: i18n.t('validation.enter-property', { arg: i18n.t('common.url') }),
-							},
-						],
-						initialValue: url || '',
-					})(<span style={{ wordBreak: 'break-all' }}>{url}</span>)}
-				</Form.Item>
-				<Modal onCancel={onCancel} onOk={onOk} visible={visible}>
-					<Form.Item label={i18n.t('common.url')} colon={false}>
-						<Input
-							defaultValue={url}
-							onChange={e => {
-								this.setState({ tempUrl: e.target.value });
-							}}
-						/>
-					</Form.Item>
-				</Modal>
-			</React.Fragment>
-		);
-	}
+  const { url, visible } = state;
+  return (
+    <>
+      <Form.Item
+        name="url"
+        label={label}
+        colon={false}
+        rules={[{ required: true }]}
+        message={i18n.t('validation.enter-property', { arg: i18n.t('common.url') })}
+        initialValue={url || ''}
+      >
+        <span style={{ wordBreak: 'break-all' }}>{url}</span>
+      </Form.Item>
+      <Modal onCancel={onCancel} onOk={onOk} visible={visible}>
+        <Form.Item label={i18n.t('common.url')} colon={false}>
+          <Input
+            defaultValue={url}
+            onChange={e => {
+              setState({ ...state, tempUrl: e.target.value });
+            }}
+          />
+        </Form.Item>
+      </Modal>
+    </>
+  );
 }
+
+UrlModal.propTypes = {
+  value: PropTypes.any,
+  onChange: PropTypes.func,
+  form: PropTypes.any,
+};
 
 export default UrlModal;
