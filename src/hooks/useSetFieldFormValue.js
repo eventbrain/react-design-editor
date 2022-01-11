914121edd91bacf4243b9a4f5e14350c4732501a
Andres Mestra <andres-mestra@users.noreply.github.com>
2022-01-11 08:30:48 -0500

    fix: error when reading property

--- src/hooks/useSetFieldFormValue.js
+++ src/hooks/useSetFieldFormValue.js
@@ -1,8 +1,9 @@
 export const useSetFieldFormValue = form => {
 	const onChangeInput = e => {
 		const value = e.target?.value;
-		const key = e.target.id ?? e.target?.name;
+		const key = e.target?.id ?? e.target?.name;
 		form.setFieldsValue({ [key]: value });
 	};
+
 	return [onChangeInput];
 };
